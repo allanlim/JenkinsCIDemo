fe9696a84735647f092e15084e7d403ee6b41f96
Allan Lim <allangators@gmail.com>
2019-02-22 10:58:03 -0500

    Update TestClass.cs

--- JustSampleTests/JustSampleTests/TestClass.cs
+++ JustSampleTests/JustSampleTests/TestClass.cs
@@ -37,7 +37,7 @@ namespace JustSampleTests
         public void TestMethodPass()
         {
             // TODO: Add test code here
-            Assert.Fail("This test has failed!");
+            Assert.Pass("This test has passed!");
         }
     }       
 }
